9849a4b00fdd13fa56aad5bb2c99fb40a4d09b0c
Karsten Tausche <karsten@fairphone.com>
2020-03-17 13:50:59 +0100

    Remove duplicated media configurations from the build
    Media codec definitions and profiles are moved to the FP2
    device-specific project.
    
    Issue: FP2P-176
    Change-Id: I71e274e051685ef2a06f04cb9f4e065fbaf38415

--- common.mk
+++ common.mk
@@ -695,20 +695,6 @@ PRODUCT_COPY_FILES += \
     system/bluetooth/data/input.conf:system/etc/bluetooth/input.conf \
     system/bluetooth/data/network.conf:system/etc/bluetooth/network.conf \
 
-PRODUCT_COPY_FILES += \
-    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
-    device/qcom/common/media/media_profiles.xml:system/etc/media_profiles.xml \
-    device/qcom/common/media/media_codecs.xml:system/etc/media_codecs.xml
-
-ifeq ($(TARGET_DEVICE_NAME_LOW_END),true)
-PRODUCT_COPY_FILES += \
-    frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:system/etc/media_codecs_google_video_le.xml
-else
-PRODUCT_COPY_FILES += \
-    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml
-endif
-
 # enable overlays to use our version of
 # source/resources etc.
 DEVICE_PACKAGE_OVERLAYS += device/qcom/common/device/overlay
