6ec15dbd0d4778bd4b959b306e285fac7c8995f0
Bharath <bharath@teamb58.org>
2020-10-03 12:51:33 +0530

    Don't build extra jars
    Attempting to build these extra jars causes build to break.
    Remove it as it is not important.
    
    Change-Id: I783dc9970648ef751663c4f8765af53d1ad8f70e

--- build/tasks/vendor_bootjars.mk
+++ build/tasks/vendor_bootjars.mk
@@ -4,9 +4,9 @@
 ifneq ($(call is-vendor-board-platform,QCOM),true)
 
 #add extra jars here
-PRODUCT_BOOT_JARS += tcmiface
+#PRODUCT_BOOT_JARS += tcmiface
 
 #call dex_preopt.mk for extra jars
-include $(BUILD_SYSTEM)/dex_preopt.mk
+#include $(BUILD_SYSTEM)/dex_preopt.mk
 
 endif
