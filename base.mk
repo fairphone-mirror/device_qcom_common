5e4e783baf833de94af6b318256f911a4c8b81f2
haibiao.lu <haibiao.lu@t2mobile.com>
2023-04-28 14:07:07 +0800

    [FP5-896]CtsDevicePolicyManagerTestCases fail
    * Root Cause
      do not need device_id_attestation feature
    * Solution
      remove device_id_attestation feature
    * Test Steps
      run cts  -m  CtsDevicePolicyManagerTestCases
    * Test Result
      pass
    
    Change-Id: I24ae98d5c485f1ec3c54812b354ca1bdbec60f0b

--- base.mk
+++ base.mk
@@ -1071,8 +1071,9 @@ PRODUCT_COPY_FILES := \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
     frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.host.xml \
-    frameworks/native/data/etc/android.software.device_id_attestation.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.device_id_attestation.xml \
     frameworks/native/data/etc/android.software.verified_boot.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.verified_boot.xml
+
+#frameworks/native/data/etc/android.software.device_id_attestation.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.device_id_attestation.xml
 
 ifneq ($(TARGET_NO_TELEPHONY), true)
 PRODUCT_COPY_FILES += \
