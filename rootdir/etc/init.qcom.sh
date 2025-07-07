33eab25ef94368d362bf8baba13bfd202e6ada65
liquan.zhou <liquan.zhou@t2mobile.com>
2025-07-07 17:24:36 +0800

    FPS-3113[1] enable_diag_and_qdss_in_user_build
    * Comment: Enable diag and qdss in user build
    * Root Cause: Requirement
    * Solution: Refer to KBA-220801033411_REV_7_How_to_enable_diag_and_qdss_in_user_build.pdf
    * Test Steps: Check relevant files and interfaces
    * Test Result: Pass
    
    Change-Id: Ibf03b2127f3956409387c0cfbc93e2df71134427
    (cherry picked from commit 5ad19151fbe19b4275e20988542496f7eb1df0ed)

--- rootdir/etc/init.qcom.sh
+++ rootdir/etc/init.qcom.sh
@@ -450,6 +450,12 @@ fi
 chmod g-w /data/vendor/modem_config
 setprop ro.vendor.ril.mbn_copy_completed 1
 
+mkdir /config/stp-policy/coresight-stm:p_ost.policy
+chmod 660 /config/stp-policy/coresight-stm:p_ost.policy
+mkdir /config/stp-policy/coresight-stm:p_ost.policy/default
+chmod 660 /config/stp-policy/coresight-stm:p_ost.policy/default
+echo 0x10 > /sys/bus/coresight/devices/coresight-stm/traceid
+
 #check build variant for printk logging
 #current default minimum boot-time-default
 buildvariant=`getprop ro.build.type`
